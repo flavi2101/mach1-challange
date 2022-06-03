522d4146f1262bd2af944e61d4ce5a1e23eef845
Flavio Henrique Ferreira <flavi2101@gmail.com>
2022-06-02 21:23:19 -0300

    menu and input settings

---
+++ index.js
@@ -0,0 +1,104 @@
+const menu = document.querySelector("[data-menu]");
+const nome = document.querySelector("[data-user = name]");
+const email = document.querySelector("[data-user = email]");
+const cpf = document.querySelector("[data-user = cpf]");
+const curso = document.querySelector("[data-user = course]");
+const btnCadastro = document.querySelector("[data-btn_cadastrar]");
+const tableBody = document.getElementById("table-users");
+var usuarios = {};
+
+menu.addEventListener("click", menuHandler);
+nome.addEventListener("input", (e) => dataHandler(e, nome.dataset));
+email.addEventListener("input", (e) => dataHandler(e, email.dataset));
+cpf.addEventListener("input", (e) => dataHandler(e, cpf.dataset));
+curso.addEventListener("input", (e) => dataHandler(e, curso.dataset));
+btnCadastro.addEventListener("click", cadastroHandler);
+
+function menuHandler() {
+  let menuItem = document.querySelector("menu");
+  let showMenu =
+    window.getComputedStyle(menuItem).getPropertyValue("display") == "flex"
+      ? "none"
+      : "flex";
+  menuItem.style.display = showMenu;
+}
+
+function dataHandler(event, data) {
+  return (usuarios[data.user] = event.target.value);
+}
+
+function validation(field) {
+  var props = Object.keys(field);
+  var validName, validEmail, validCfp, validCurso;
+  props.forEach((key) => {
+    switch (key) {
+      case "name":
+        validName = isValidText(field.name);
+        break;
+      case "email":
+        validEmail = isValidEmail(field.email);
+        break;
+      case "cpf":
+        validCfp = isValidCpf(field.cpf);
+        break;
+      case "course":
+        validCurso = isValidText(field.course);
+        break;
+    }
+  });
+  return validCfp && validCurso && validEmail && validName;
+}
+
+function isValidText(value) {
+  var regex = /^[a-zA-Z ]{2,30}$/;
+  var teste = regex.test(value);
+  showError(value, teste);
+  return teste;
+}
+
+function isValidEmail(email) {
+  var regex =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  var teste = regex.test(String(email).toLowerCase());
+  showError(email, teste);
+  return teste;
+}
+
+function isValidCpf(cpf) {
+  var regex =
+    /^([0-9]{3}\.?[0-9]{3}\.?[0-9]{3}\-?[0-9]{2}|[0-9]{2}\.?[0-9]{3}\.?[0-9]{3}\/?[0-9]{4}\-?[0-9]{2})$/;
+  var teste = regex.test(Number(cpf));
+  showError(cpf, teste);
+  return teste;
+}
+
+function showError(value, teste) {
+  if (teste == false) {
+    return window.alert(`${value} is invalid`);
+  }
+}
+
+function cadastroHandler(e) {
+  e.preventDefault();
+  var allFields = Object.keys(usuarios).length;
+  console.log(allFields);
+  if (allFields != 4) {
+    window.alert("Não é permitido campo em branco");
+  } else if (validation(usuarios) == true) {
+    let dataUser = document.createElement("tr");
+    dataUser.innerHTML = `
+    <td>${usuarios.name}</td>
+    <td>${usuarios.email}</td>
+    <td>${usuarios.cpf}</td>
+    <td>${usuarios.course}</td>
+    <td data-delete><img src="./src/img/content_img/close-red.svg" alt="close" width="15" height="15"></td>`;
+
+    nome.value = "";
+    email.value = "";
+    cpf.value = "";
+    curso.value = "";
+    usuarios = {};
+
+    return tableBody.appendChild(dataUser);
+  }
+}
